061349bd87d098b626f5e7d96b59ab6e68ca7474
Petrik Diak <diak@petrik.hu>
2024-06-12 10:07:38 +0200

    added client interface

---
+++ palinka-project-prototype/src/Client.ts
@@ -0,0 +1,9 @@
+export interface Client{
+    id?: number,
+    adoszam: string,
+    name: string,
+    lakcim: string,
+    fozes_mennyiseg: number,
+    fozes_ideje: number,
+    feldolgozott_cefre: number 
+}
